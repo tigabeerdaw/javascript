81f2159a0e09907dd5455a22ca0471fca8c9acee
tigabeerdaw <tigabe.2114@gmail.com>
2024-12-14 12:03:55 +0300

    fldlf

--- xss_prev.js
+++ xss_prev.js
@@ -3,4 +3,6 @@ function userInput(user_input) {
   const sanitized_input = DOMPurify.sanitize(user_input);
   document.getElementById("output").innerHTML = sanitizedInput;
   
+  
+  console.log("try xss");
 }
